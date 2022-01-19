8ecbe9ee33912a1781af102a5cefe8934f9b8d6d
Tomas Rodríguez <trodrigueza@unal.edu.co>
2022-01-18 19:53:34 -0500

    dynamic site

---
+++ 189. Dynamic Web Page/index.php
@@ -0,0 +1,15 @@
+<?php
+$i = rand(0, 99999999);
+if($i % 1332 == 0){
+  echo ("Random value is " . $i);
+} else {
+  while($i % 1332 != 0) {
+    $i = (rand(0, 99999999));
+    if($i % 1332 == 0){
+      echo ("Random value is " . $i);
+    };
+  };
+};
+?>
+
+<p></p>
